5d7242334dd5549d2d46017facccbfd1d38d691b
David <davidmontesvilanova@gmail.com>
2025-09-25 19:34:27 -0700

    Data retrieval functionality

--- classHandler/clas.py
+++ classHandler/clas.py
@@ -5,17 +5,17 @@ class Section:
         self.name = name.strip()
         self.credits = credits
         self.section = section
-        self.formatTime(self, time)
+        self.formatTime(time)
         self.location = location.strip()
         self.instructor = instructor.strip()
         self.seats_taken = seats_taken
         self.seats_total = seats_total
 
     def __repr__(self):
-        return f"Section(code={self.code}, name={self.name}, credits={self.credits}, section={self.section}, days={self.days}, time={self.time}, location={self.location}, instructor={self.instructor}, seats_taken={self.seats_taken}, seats_total={self.seats_total})"
+        return f"Section(code={self.code}, name={self.name}, credits={self.credits}, section={self.section}, days={self.days}, location={self.location}, instructor={self.instructor}, seats_taken={self.seats_taken}, seats_total={self.seats_total})"
     
     def formatTime(self, time) -> None:
-        if self.time == "AARGT":
+        if time == "AARGT":
             return "N/A"
         
         # format time from (example) "TU,THU12.30-13.30" to something like "(Tuesday, Thursday), "12:30 PM - 1:30 PM""
--- dataHandler/scraper.py
+++ dataHandler/scraper.py
@@ -1,7 +1,8 @@
 #from classHandler.semester import Semester
 #from classHandler.subject import Subject    
 from classHandler.clas import Section
-#from classHandler.campus import Campus
+from classHandler.campus import Campus
+import os
 import time
 import json
 from bs4 import BeautifulSoup
@@ -11,17 +12,20 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
 from webdriver_manager.chrome import ChromeDriverManager   
 
 
 class Scraper:
     
     _instance = None
-
     _options = Options()
-    #_options.add_argument("--headless")
+    _options.add_argument("--headless=new")
     _options.add_argument("--disable-gpu")
     _options.add_argument("--window-size=1920,1080")
+    _options.add_experimental_option('excludeSwitches', ['enable-logging'])
+    _devnull = os.devnull  # 'NUL' on Windows
+    _service = Service(ChromeDriverManager().install(), log_path=_devnull)
 
     def __new__(cls):
         if cls._instance is None:
@@ -33,139 +37,160 @@ class Scraper:
             self.initialized = True
 
     @classmethod
-    def fetch_data(cls, url) -> BeautifulSoup: 
+    def fetch_data(cls, url) -> list: 
+        college_info = []
+        semester_info = []
+        subject_info = []
         try:
-            driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=cls._options)
+            driver = webdriver.Chrome(service=cls._service, options=cls._options)
+            action = ActionChains(driver)
+
             driver.get(url)
             WebDriverWait(driver, 10).until(
                 EC.presence_of_element_located((By.CLASS_NAME, "schedule-container")) # Wait for content to load (Content that contains the campuses, which inturn contains everything else)
             )
 
             html = driver.page_source
-            soup = BeautifulSoup(html, 'html.parser').find("div", class_="schedule-container")  # Get the main container that holds all the data
-
-            semesterSelection = driver.current_window_handle
+            soup = BeautifulSoup(html, "html.parser").find("div", class_="schedule-container")  # Get the main container that holds all the data
             
-            campus = cls.getCampus(soup)
-            """
-            After we get the campuses, let the user select what campus they want. Then we get the semesters for a given campus. Then we get the subjects. Then we get the classess in a subject
+            campuses = cls.getCampuses(soup)
 
-            """
-            campus_name = campus.find("h4", class_="City").text.strip()
-            print(f"Campus selected: {campus_name}")
 
-            semester = cls.getSemester(campus)
-            print(f"Semester selected: {semester}")
 
-            buttonToClick = driver.find_element(By.CSS_SELECTOR, f"a.nav-main[aria-label='{campus_name + semester.replace(" ", "")}']")
-            buttonToClick.click()  # Click on the semester to load the subjects
-            WebDriverWait(driver, 10).until(
-                EC.presence_of_element_located((By.CSS_SELECTOR, "table.wsu-c-table.wsu-c-table--striped.soc-table tbody tr"))  # Wait for the table that contains the subjects to load
-            )
+            for campus in campuses:
+                campus_name = campus.find("h4", class_="City").text.strip()
 
-            html = driver.page_source
-            soup = BeautifulSoup(html, 'html.parser').find("tbody", class_=["wsu-c-table", "wsu-c-table--striped"])  # Get the table that contains the subjects
-            
-            classSelection = driver.current_window_handle
+                #Very bad temp (or perm) solution
+                if campus_name == "Global":
+                    campus_name = "DDP"
+                
+                print(f"Campus selected: {campus_name}")
+                semesters = cls.getSemesters(campus)
 
-            subjects = cls.getSubjects(soup) 
-            print(f"Subject selected: {subjects[0][1]}") # Just gonna get the first subject for now
 
-            buttonToClick = driver.find_element(By.CSS_SELECTOR, f"a[aria-label='{subjects[0][1]}']")
-            buttonToClick.click()
+                for semester in semesters:
+                    print(f"Semester selected: {semester}")
 
-            WebDriverWait(driver, 10).until(
-                EC.presence_of_element_located((By.CLASS_NAME, "tocsvbutton"))  # Wait for the button that lets us access the csv to load
-            )
+                    WebDriverWait(driver, 10).until(
+                        EC.presence_of_element_located((By.CSS_SELECTOR, f"a.nav-main[aria-label=\"{campus_name + semester.replace(" ", "")}\"]"))  # Wait for the table that contains the subjects to load
+                    )
 
-            html = driver.page_source
-            soup = BeautifulSoup(html, 'html.parser').find("a", class_="tocsvbutton")  
-            href = soup['href'].replace("tocsv", "") # We do not want the csv, we want the raw html data
-            driver.get(href)
+                    buttonToClick = driver.find_element(By.CSS_SELECTOR, f"a.nav-main[aria-label=\"{campus_name + semester.replace(" ", "")}\"]")
+                    buttonToClick.click()  # Click on the semester to load the subjects
+                    WebDriverWait(driver, 10).until(
+                        EC.presence_of_element_located((By.CSS_SELECTOR, "table.wsu-c-table.wsu-c-table--striped.soc-table tbody tr"))  # Wait for the table that contains the subjects to load
+                    )
 
-            WebDriverWait(driver, 10).until(
-                EC.presence_of_element_located((By.CLASS_NAME, "json-formatter-container"))  # Wait for the table that contains the classes to load
-            )
+                    html = driver.page_source
+                    soup = BeautifulSoup(html, "html.parser").find("tbody", class_=["wsu-c-table", "wsu-c-table--striped"])  # Get the table that contains the subjects
 
-            html = driver.page_source
-            soup = BeautifulSoup(html, 'html.parser').find("pre")
-            classes = json.loads(soup.text)["sections"]  # Get the json data that contains the classes
-            classes = cls.formatClasses(classes)  # Format the classes into Section objects
-            
-            with open("Output.txt", "w", encoding="utf-8") as file:
-                 for c in classes:
-                    file.write(str(c))
-                    file.write("\n")
+                    subjects = cls.getSubjects(soup) 
 
+
+                    for name, title in subjects:
+                        print(f"Subject selected: {name.strip()} - {title.strip()}")
+
+                        WebDriverWait(driver, 10).until(
+                            EC.presence_of_element_located((By.CSS_SELECTOR, f"a[aria-label=\"{title}\"]")) 
+                        )
+
+                        buttonToClick = driver.find_element(By.CSS_SELECTOR, f"a[aria-label=\"{title}\"]")
+
+                        WebDriverWait(driver, 10).until(
+                            EC.element_to_be_clickable(buttonToClick)
+                        )
+
+                        try:
+                            action.move_to_element(buttonToClick).click().perform()
+                        except Exception as e:
+                            print(f"Error getting {name} data")
+                            continue
+
+                        WebDriverWait(driver, 10).until(
+                            EC.presence_of_element_located((By.CLASS_NAME, "tocsvbutton"))  # Wait for the button that lets us access the csv to load
+                        )
+
+                        html = driver.page_source
+                        soup = BeautifulSoup(html, "html.parser").find("a", class_="tocsvbutton")  
+                        href = soup['href'].replace("tocsv", "") # We do not want the csv, we want the raw html data
+                        driver.get(href)
+
+                        WebDriverWait(driver, 10).until(
+                            EC.presence_of_element_located((By.CLASS_NAME, "json-formatter-container"))  # Wait for the table that contains the classes to load
+                        )
+
+                        html = driver.page_source
+                        soup = BeautifulSoup(html, "html.parser").find("pre")
+                        classes = json.loads(soup.text)["sections"]  # Get the json data that contains the classes
+                        classes_l = cls.formatClasses(classes)  # Format the classes into Section objects
+                        subject_info.append(classes_l)
+
+                        driver.back()
+                        driver.back() # go back to subject selection page
+                        WebDriverWait(driver, 10).until(
+                            EC.presence_of_element_located((By.CSS_SELECTOR, "table.wsu-c-table.wsu-c-table--striped.soc-table tbody tr"))  # Wait for the table that contains the subjects to load
+                        )
+
+                        html = driver.page_source
+                        soup = BeautifulSoup(html, 'html.parser').find("tbody", class_=["wsu-c-table", "wsu-c-table--striped"])  # Get the table that contains the subjects
+                        semester_info.append(subject_info)
+                    driver.back()  # Go back to the campus/semester selection page
+                    college_info.append(Campus(campus_name, semester_info))
+                    semester_info.clear()
+                    subject_info.clear()
+
+            print("Data fetching complete.")
+            time.sleep(10)
             driver.quit()
-            return soup
         except Exception as e:
             print(f"An error occurred in the data fetching process: {e}")
-            return None
+        with open("Output.txt", "w", encoding="utf-8") as file:
+            file.write(str(college_info))
+        return college_info
         
         
     # get the campus that the user wants to select, reducing the htmlData to just info about the campus
     @classmethod
-    def getCampus(cls, htmlData: BeautifulSoup) -> BeautifulSoup:
+    def getCampuses(cls, htmlData: BeautifulSoup) -> list:
         campus_info = htmlData.find_all("div", class_="header_wrapper") # Get all wrappers that contain campus names and semesters
         campuses = []
 
         for campus in campus_info:
             campus_name = campus.find("h4", class_="City")
             campuses.append(campus_name.text.strip())
-        
-        #inpt = input(f"Available campuses: {', '.join(campuses)}\nPlease select a campus: ")
-        inpt = "pullman" # Remove when ready
 
-        while inpt.lower() not in [c.lower() for c in campuses]:
-            print("Invalid campus selected. Please try again.")
-            inpt = input(f"Available campuses: {', '.join(campuses)}\nPlease select a campus: ")
-        inpt = next((c for c in campuses if c.lower() == inpt.lower()), None)
-
-        for campus in campus_info:
-            if campus.find("h4", class_="City").text.strip().lower() == inpt.lower():
-                return campus
+        return campus_info
     
     @classmethod
-    def getSemester(cls, campusData: BeautifulSoup) -> str: #Return string because there is no information nested under semesters - it is a link that changes the page
+    def getSemesters(cls, campusData: BeautifulSoup) -> list: #Return string because there is no information nested under semesters - it is a link that changes the page
         semestersList = campusData.find("ul", class_="Semesters").find_all("li", class_="nav-item semester")
-        semesters = [semester.find("a", class_="nav-main") for semester in semestersList]
-        #print(f"Available semesters: {', '.join([sem.text.strip() for sem in semesters])}")
+        semesters = [semester.find("a", class_="nav-main").text.strip() for semester in semestersList]
 
-        #inpt = input(f"Available semesters: {', '.join([sem.text.strip() for sem in semesters])}\nPlease select a semester: ")
-        inpt = "Spring 2025"
-        while inpt not in [sem.text.strip() for sem in semesters]:
-            print("Invalid semester selected. Please try again.")
-            inpt = input(f"Available semesters: {', '.join([sem.text.strip() for sem in semesters])}\nPlease select a semester: ")
-
-        inpt = next((sem.text.strip() for sem in semesters if sem.text.strip() == inpt), None)
-        for semester in semesters:
-            if semester.text.strip() == inpt:
-                return semester.text.strip()
+        return semesters
             
     @classmethod
     def getSubjects(cls, semesterData: BeautifulSoup) -> list:
         subjectsRaw = semesterData.find_all("tr", class_="zebratable")
         subjects = []
         for subject in subjectsRaw:
-            subject_name = subject.find("td", class_="zebratablesubject").text.strip()
-            subject_title = subject.find("td", class_="zebratabletitle").text.strip()
-            subjects.append((subject_name, subject_title, subject))
+            subject_name = subject.find("td", class_="zebratablesubject").text
+            subject_title = subject.find("td", class_="zebratabletitle").text
+            subjects.append((subject_name, subject_title))
         return subjects
     
     @classmethod
     def formatClasses(cls, classData: list) -> list:
         reducedClasses = []
-        for c in classData:
-            code = c["sln"]
-            name = c["title"]
-            credits = c["credits"]
-            section = c["sectionNumber"]
-            t = c["dayTime"]
-            location = c["location"]
-            instructor = c["instructor"]
-            seats_taken = c["enrollment"]
-            seats_total = c["enrollmentLimit"]
+        for clas in classData:
+            code = clas["sln"]
+            name = clas["title"]
+            credits = clas["credits"]
+            section = clas["sectionNumber"]
+            t = clas["dayTime"]
+            location = clas["location"]
+            instructor = clas["instructor"]
+            seats_taken = clas["enrollment"]
+            seats_total = clas["enrollmentLimit"]
 
             reducedClasses.append(Section(code, name, credits, section, t, location, instructor, seats_taken, seats_total))
         return reducedClasses
