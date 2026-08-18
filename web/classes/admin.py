72158279c63b4150e60115130b0d5bd31a99a3be
NinjaO9 <156165726+NinjaO9@users.noreply.github.com>
2026-08-17 19:09:18 -0800

    Remove custom data analytics in favor of Vercel provided analytics (#5)
    https://github.com/NinjaO9/Crimson-Scheduler/pull/5

--- web/classes/admin.py
+++ web/classes/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Campus, Semester, Topics, Course, Section, DailyAnalyticsMetric
+from .models import Campus, Semester, Topics, Course, Section
 # Register your models here.
 
 admin.site.register(Campus)
@@ -7,12 +7,4 @@ admin.site.register(Semester)
 admin.site.register(Topics)
 admin.site.register(Course)
 admin.site.register(Section)
-
-
-@admin.register(DailyAnalyticsMetric)
-class DailyAnalyticsMetricAdmin(admin.ModelAdmin):
-    list_display = ('event_date', 'event_name', 'count', 'updated_at')
-    list_filter = ('event_name', 'event_date')
-    ordering = ('-event_date', 'event_name')
-    readonly_fields = ('event_date', 'event_name', 'count', 'updated_at')
 
