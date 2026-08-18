2be7b826c9660b4e563d92859c77ea0cae41c819
David <davidmontesvilanova@gmail.com>
2026-08-18 10:58:33 -0800

    Repair missing favicon (all devices)

--- web/web/urls.py
+++ web/web/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('favicon.ico', RedirectView.as_view(url='/static/icons/favicon.ico', permanent=True)),
     path('', include("classes.urls")),
 ]
